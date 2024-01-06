3e8548e8f573be689b4ec8ccd8945cf5695b3284
salah <salahuznr@gmail.com>
2024-01-06 14:20:22 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {BrowserRouter,Link,Route,Routes} from 'react-router-dom'
+
+import {logo} from './assets'
+// import {Home,createPost} from './page'
 
 const App = () => {
   return (
-    <h1 class="text-3xl font-bold underline">
-    salalsckaksfksdvsvlsvs
-  </h1>
+    <BrowserRouter>
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+    <Link to="/">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+      </Link>
+    </header>
+    </BrowserRouter>
+
   )
 }
 
