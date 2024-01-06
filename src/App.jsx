9eb78a7d5180d9e8a6ea80b9db775ce9868247e0
salah <salahuznr@gmail.com>
2024-01-06 14:39:52 +0530

    home added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter,Link,Route,Routes} from 'react-router-dom'
 
 import {logo} from './assets'
-// import {Home,createPost} from './page'
+import {Home,CreatePost} from './page'
 
 const App = () => {
   return (
@@ -11,7 +11,16 @@ const App = () => {
     <Link to="/">
         <img src={logo} alt="logo" className="w-28 object-contain" />
       </Link>
+      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">
+        Create
+      </Link>
     </header>
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </main>
     </BrowserRouter>
 
   )
