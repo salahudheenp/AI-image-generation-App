0b4002719d604faa7493f088f3177d52dfa79844
salah <salahuznr@gmail.com>
2024-01-10 18:50:31 +0530

    error solved

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter,Link,Route,Routes} from 'react-router-dom'
 
 import {logo} from './assets'
-import {Home,CreatePost} from './page'
+// import {Home,CreatePost} from './page'
 
 const App = () => {
   return (
@@ -15,10 +15,10 @@ const App = () => {
         Create
       </Link>
     </header>
-    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh - 73px)]">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-post" element={<CreatePost />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/create-post" element={<CreatePost />} /> */}
       </Routes>
     </main>
     </BrowserRouter>
