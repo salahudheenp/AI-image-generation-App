fef0f9ebc93cb22555a95095db761cba7f352f29
salah <salahuznr@gmail.com>
2024-01-11 17:21:54 +0530

    tailwind config changed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     <Link to="/">
         <img src={logo} alt="logo" className="w-28 object-contain" />
       </Link>
-      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">
+      <Link to="/create-post" className="text-1xl font-bold underline">
         Create
       </Link>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-
-module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      screens: {
-        xs: '480px',
-      },
-      fontFamily: {
-        inter: ['Inter var', 'sans-serif'],
-      },
-      boxShadow: {
-        card: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.2)',
-        cardhover: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.4)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
