15746ff75ec89dde4960f92e91b16abc8b6f7039
salah <salahuznr@gmail.com>
2024-01-12 16:58:16 +0530

    config.js changed to .cjs

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter,Link,Route,Routes} from 'react-router-dom'
 
 import {logo} from './assets'
-// import {Home,CreatePost} from './page'
+import {Home} from './page'
 
 const App = () => {
   return (
@@ -11,13 +11,14 @@ const App = () => {
     <Link to="/">
         <img src={logo} alt="logo" className="w-28 object-contain" />
       </Link>
-      <Link to="/create-post" className="text-1xl font-bold underline">
+      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">
         Create
       </Link>
+  
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh - 73px)]">
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
         {/* <Route path="/create-post" element={<CreatePost />} /> */}
       </Routes>
     </main>
