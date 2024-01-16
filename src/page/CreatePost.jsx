e7ebdb54cae45239616741ea684921b8aceb0e8f
salah <salahuznr@gmail.com>
2024-01-16 20:30:00 +0530

    state-createPost

--- src/page/CreatePost.jsx
+++ src/page/CreatePost.jsx
@@ -6,6 +6,22 @@ import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
+  
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
   return (
     <div>CreatePost</div>
   )
