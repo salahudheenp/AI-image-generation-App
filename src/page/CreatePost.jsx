1f653951ea5a249c0f647293e9b34c6d68f9a105
salah <salahuznr@gmail.com>
2024-01-17 19:22:45 +0530

    committ

--- src/page/CreatePost.jsx
+++ src/page/CreatePost.jsx
@@ -1,7 +1,15 @@
-
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import React from 'react'
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
   return (
     <div>CreatePost</div>
   )
