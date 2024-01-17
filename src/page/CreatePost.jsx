4f65bbd44f4f57a7e317a9cacb1a50843bfc6567
salah <salahuznr@gmail.com>
2024-01-17 19:23:15 +0530

    states

--- src/page/CreatePost.jsx
+++ src/page/CreatePost.jsx
@@ -10,6 +10,18 @@ const CreatePost = () => {
     prompt: '',
     photo: '',
   });
+
+  
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
+
   return (
     <div>CreatePost</div>
   )
