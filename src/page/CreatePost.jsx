156ad46143ba2f76e128b6a23048e03260f56f2a
salah <salahuznr@gmail.com>
2024-01-27 14:16:08 +0530

    utils added

--- src/page/CreatePost.jsx
+++ src/page/CreatePost.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import {getRandomPrompt} from '..'
+
 
 const CreatePost = () => {
   return (
