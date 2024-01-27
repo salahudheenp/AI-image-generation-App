4029101c0f47f7f64655126f75b2bed8ec539682
salah <salahuznr@gmail.com>
2024-01-27 14:08:50 +0530

    create-ppoost

--- src/page/CreatePost.jsx
+++ src/page/CreatePost.jsx
@@ -1,27 +1,6 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import React from 'react'
 
 const CreatePost = () => {
-  const navigate = useNavigate();
-
-  const [form, setForm] = useState({
-    name: '',
-    prompt: '',
-    photo: '',
-  });
-
-  
-  const [generatingImg, setGeneratingImg] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
-
-  const handleSurpriseMe = () => {
-    const randomPrompt = getRandomPrompt(form.prompt);
-    setForm({ ...form, prompt: randomPrompt });
-  };
-
   return (
     <div>CreatePost</div>
   )
