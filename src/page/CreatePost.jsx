519d4690334f627390e35dd7deac153c96a766c8
salah <salahuznr@gmail.com>
2024-01-27 17:03:26 +0530

    fff

--- src/page/CreatePost.jsx
+++ src/page/CreatePost.jsx
@@ -6,6 +6,29 @@ import {getRandomPrompt} from '..'
 
 
 const CreatePost = () => {
+
+
+
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
+
+
+  
   return (
     <div>CreatePost</div>
   )
