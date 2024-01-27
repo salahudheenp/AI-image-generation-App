377936cbc4d7b1f5ab45c22464e9b23515a3ca09
salah <salahuznr@gmail.com>
2024-01-27 17:05:27 +0530

    pro mpt

--- src/page/CreatePost.jsx
+++ src/page/CreatePost.jsx
@@ -53,7 +53,34 @@ const CreatePost = () => {
     }
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...form }),
+        });
+
+        await response.json();
+        alert('Success');
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
+  };
+
+  
   return (
     <div>CreatePost</div>
   )
