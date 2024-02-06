350aedaea5e153cb7b9b6964888c107cb20d3582
salah <salahuznr@gmail.com>
2024-02-06 19:56:22 +0530

    create post

--- src/page/CreatePost.jsx
+++ src/page/CreatePost.jsx
@@ -80,7 +80,34 @@ const CreatePost = () => {
 
   
   return (
-    <div>CreatePost</div>
+    <section className="max-w-7xl mx-auto">
+    <div>
+      <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+      <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
+    </div>
+
+    <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+      <div className="flex flex-col gap-5">
+      <FormField
+            labelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="Ex., john doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+
   )
 }
 
