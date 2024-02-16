d086d14c8cc411588d3afec2f976bfd724abd7ef
salah <salahuznr@gmail.com>
2024-02-16 16:41:15 +0530

    formata

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,15 @@
-import FileSaver from 'file-saver';
-import { surpriseMePrompts } from '../constant';
+import FileSaver from "file-saver";
+import { surpriseMePrompts } from "../constant";
 
 export function getRandomPrompt(prompt) {
-  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
-  const randomPrompt = surpriseMePrompts[randomIndex];
+    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+    const randomPrompt = surpriseMePrompts[randomIndex];
 
-  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+    if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
-  return randomPrompt;
+    return randomPrompt;
 }
 
 export async function downloadImage(_id, photo) {
-  FileSaver.saveAs(photo, `download-${_id}.jpg`);
+    FileSaver.saveAs(photo, `download-${_id}.jpg`);
 }
