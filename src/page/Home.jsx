375067b2b9bf8275012838897c185b4a01085657
salah <salahuznr@gmail.com>
2024-02-17 16:18:56 +0530

    format

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -6,7 +6,7 @@ const RenderCards = ({ data, title }) => {
         return data.map((post) => <Card key={post._id} {...post} />);
     }
 
-    return <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>;
+    return <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase ">{title}</h2>;
 };
 
 const Home = () => {
